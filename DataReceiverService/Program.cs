5192ade3da8fa8958e4ab777a2980a732f69fb88
KonH <konh@yandex.ru>
2024-04-01 16:57:13 +0200

    DataReceiverService - healthchecks only

---
+++ DataReceiverService/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+if ( app.Environment.IsDevelopment() ) {
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.MapHealthChecks("/health");
+
+app.Run();
