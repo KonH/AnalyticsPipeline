20bdaa08df4cd04126c35447c45207f92783cc1a
KonH <konh@yandex.ru>
2024-04-18 15:54:52 +0200

    Setup Postgres database

---
+++ pg_initdb/01_setup_read_only_user.sql
@@ -0,0 +1,9 @@
+﻿CREATE USER analytics_r_user WITH PASSWORD 'analytics_r_user_password';
+
+GRANT CONNECT ON DATABASE analytics_database TO analytics_r_user;
+
+GRANT USAGE ON SCHEMA public TO analytics_r_user;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO analytics_r_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO analytics_r_user;
