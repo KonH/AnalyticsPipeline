b884103e39c7d2b23ce514ea8b98cc6c45ca30c0
KonH <konh@yandex.ru>
2024-04-19 08:57:35 +0200

    Fix formatting

--- DataReceiverService/Controllers/DataReceiverController.cs
+++ DataReceiverService/Controllers/DataReceiverController.cs
@@ -8,42 +8,44 @@ namespace DataReceiverService.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class DataReceiverController : ControllerBase {
-    readonly ILogger<DataReceiverController> _logger;
-    readonly JsonSerializerOptions _jsonSerializerOptions;
-    readonly IProducer<Null, string> _producer;
+	readonly ILogger<DataReceiverController> _logger;
+	readonly JsonSerializerOptions _jsonSerializerOptions;
+	readonly IProducer<Null, string> _producer;
 
-    public DataReceiverController(ILogger<DataReceiverController> logger) {
-        _logger = logger;
-        _jsonSerializerOptions = new JsonSerializerOptions {
-            WriteIndented = true
-        };
-        var config = new ProducerConfig {
-            BootstrapServers = "redpanda:9092"
-        };
-        _logger.LogDebug("Create Kafka producer with servers: {bootstrapServers}", config.BootstrapServers);
-        _producer = new ProducerBuilder<Null, string>(config)
-            .Build();
-    }
+	public DataReceiverController(ILogger<DataReceiverController> logger) {
+		_logger = logger;
+		_jsonSerializerOptions = new JsonSerializerOptions {
+			WriteIndented = true
+		};
+		var config = new ProducerConfig {
+			BootstrapServers = "redpanda:9092"
+		};
+		_logger.LogDebug("Create Kafka producer with servers: {bootstrapServers}", config.BootstrapServers);
+		_producer = new ProducerBuilder<Null, string>(config)
+			.Build();
+	}
 
-    [HttpPost("Handle")]
-    public async Task<IActionResult> Handle(UserEventsRequest userEvent) {
-        if ( _logger.IsEnabled(LogLevel.Debug) ) {
-            var json = JsonSerializer.Serialize(userEvent, _jsonSerializerOptions);
-            _logger.LogDebug(
-                "Received UserEventsRequest with {eventCount} events: {userEventsJson}", 
-                userEvent.Events.Count, json);
-        }
-        _logger.LogDebug("Sending {eventCount} events to message queue", userEvent.Events.Count);
-        foreach ( var eventItem in userEvent.Events ) {
-            _logger.LogDebug("Sending event {eventName}:{eventId} to message queue", eventItem.EventName, eventItem.Id);
-            var queueEvent = new QueueEvent(eventItem.Id, eventItem.Timestamp, eventItem.EventName, eventItem.Properties, userEvent.UserId);
-            var queueEventJson = JsonSerializer.Serialize(queueEvent, _jsonSerializerOptions);
-            await _producer.ProduceAsync("message-queue", new Message<Null, string> {
-                Value = queueEventJson
-            });
-            _logger.LogDebug("Event {eventName}:{eventId} to message queue sent", eventItem.EventName, eventItem.Id);
-        }
-        _logger.LogDebug("{eventCount} events sent to message queue", userEvent.Events.Count);
-        return Ok();
-    }
+	[HttpPost("Handle")]
+	public async Task<IActionResult> Handle(UserEventsRequest userEvent) {
+		if ( _logger.IsEnabled(LogLevel.Debug) ) {
+			var json = JsonSerializer.Serialize(userEvent, _jsonSerializerOptions);
+			_logger.LogDebug(
+				"Received UserEventsRequest with {eventCount} events: {userEventsJson}",
+				userEvent.Events.Count, json);
+		}
+
+		_logger.LogDebug("Sending {eventCount} events to message queue", userEvent.Events.Count);
+		foreach ( var eventItem in userEvent.Events ) {
+			_logger.LogDebug("Sending event {eventName}:{eventId} to message queue", eventItem.EventName, eventItem.Id);
+			var queueEvent = new QueueEvent(eventItem.Id, eventItem.Timestamp, eventItem.EventName, eventItem.Properties, userEvent.UserId);
+			var queueEventJson = JsonSerializer.Serialize(queueEvent, _jsonSerializerOptions);
+			await _producer.ProduceAsync("message-queue", new Message<Null, string> {
+				Value = queueEventJson
+			});
+			_logger.LogDebug("Event {eventName}:{eventId} to message queue sent", eventItem.EventName, eventItem.Id);
+		}
+
+		_logger.LogDebug("{eventCount} events sent to message queue", userEvent.Events.Count);
+		return Ok();
+	}
 }
